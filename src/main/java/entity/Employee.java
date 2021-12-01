685cee54b015fddb0a3d2392ee2b39daf9ec7c6d
clement <clement.cauliez@gmail.com>
2021-12-01 16:27:57 +0100

    first commit, created employee class + cfg + db

---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class Employee {
+
+    private String lastname;
+    private String firstname;
+    private String email;
+    private int age;
+    private String jobTitle;
+    private String phoneNumber;
+    private String address;
+}
