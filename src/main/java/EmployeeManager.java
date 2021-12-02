04a64fb6229b7d424a4aa6ecdcba3ca685afe633
clement <clement.cauliez@gmail.com>
2021-12-02 10:44:06 +0100

    added getAll function, deprecated

--- src/main/java/EmployeeManager.java
+++ src/main/java/EmployeeManager.java
@@ -6,37 +6,61 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
 public class EmployeeManager {
 
+    // prepare a session factory object and an int storing the total number of employees to be used later
     protected SessionFactory sessionFactory;
     protected int nbOfEmployees;
 
+    /**
+     * sets up the connection to the db
+     * method will create a registry object from the cfg.xml file we created
+     * will try to create a new session from that reg
+     */
     protected void setup(){
+        // create a constant from cfg file
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
 
+        // tries to create a new session from reg
         try {
             sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
         }
+        // catch potential errors, destroys the const, prints the error
         catch (Exception e){
             StandardServiceRegistryBuilder.destroy(registry);
             e.printStackTrace();
         }
     }
 
+    /**
+     * closes the session
+     */
     protected void exit(){
         sessionFactory.close();
     }
 
-    //CRUD
+    /**
+     * function opens the session and sends a transaction, a query to the db to add a new line in it
+     * @param employee the object to be inserted into the db
+     */
     protected void create(Employee employee){
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         session.save(employee);
         session.getTransaction().commit();
         session.close();
+        //increases the total employee counter
         nbOfEmployees ++;
     }
 
+    /**
+     * function that wants to get an object(line) from the table
+     * @param id id of the object we wish to get info of
+     * @return the employee object
+     */
     protected Employee read(long id){
         Session session = sessionFactory.openSession();
         Employee employee = session.get(Employee.class, id);
@@ -44,8 +68,15 @@ public class EmployeeManager {
         return employee;
     }
 
+    /**
+     * function to change the attributed values of an object in the db
+     * @param id id of the object we want to modify
+     * @param employee new object with values we want to modify the target object to
+     */
     protected void update(long id, Employee employee){
+        // store the employee to be modified
         Employee oldEmployee = this.read(id);
+        // check if the value is to be modified by checking for nullity
         if(employee.getAddress() != null){
             oldEmployee.setAddress(employee.getAddress());
         }
@@ -74,6 +105,10 @@ public class EmployeeManager {
         session.close();
     }
 
+    /**
+     * function to delete an entry from the database
+     * @param employee object to delete
+     */
     protected void delete(Employee employee){
         Session session = sessionFactory.openSession();
         session.beginTransaction();
@@ -82,8 +117,12 @@ public class EmployeeManager {
         session.close();
     }
 
+    /**
+     * function to delete all objects from the database
+     */
     protected void deleteAll(){
         Session session = sessionFactory.openSession();
+        // loops through the employees through their PK, probably really not safe? idk
         for (int i = 1; i <= nbOfEmployees; i++) {
             if (read(i) != null) {
                 session.beginTransaction();
@@ -94,14 +133,26 @@ public class EmployeeManager {
         session.close();
     }
 
+    /**
+     * Create a list of all employees
+     * @return a list of all employees
+     */
+    protected List<Employee> getAll(){
+        Session session = sessionFactory.openSession();
+        // queries the session to create a list of objects that satisfy the criteria of belonging to the employee class
+        List<Employee> employeeList = session.createCriteria(Employee.class).list();
+        session.close();
+        return employeeList;
+    }
+
     public static void main(String[] args) {
-//        EmployeeManager employeeManager = new EmployeeManager();
-//        Employee employee = EmployeeFactory.createEmployee();
-//        Employee employee2 = EmployeeFactory.createEmployee();
-//        Employee employee3 = EmployeeFactory.createEmployee();
-//        Employee employee4 = EmployeeFactory.createEmployee();
-//        Employee employee5 = EmployeeFactory.createEmployee();
-//        employeeManager.setup();
+        EmployeeManager employeeManager = new EmployeeManager();
+        Employee employee = EmployeeFactory.createEmployee();
+        Employee employee2 = EmployeeFactory.createEmployee();
+        Employee employee3 = EmployeeFactory.createEmployee();
+        Employee employee4 = EmployeeFactory.createEmployee();
+        Employee employee5 = EmployeeFactory.createEmployee();
+        employeeManager.setup();
 //        employeeManager.create(employee);
 //        employeeManager.create(employee2);
 //        employeeManager.create(employee3);
@@ -114,6 +165,9 @@ public class EmployeeManager {
 //        Employee employee = employeeManager.read(id);
 //        employeeManager.delete(employee);
 //        employeeManager.deleteAll();
-//        employeeManager.exit();
+        System.out.println(employeeManager.getAll());
+        employeeManager.exit();
+
+
     }
 }
--- src/main/java/factory/EmployeeFactory.java
+++ src/main/java/factory/EmployeeFactory.java
@@ -5,30 +5,42 @@ import entity.Employee;
 public class EmployeeFactory {
 
 
+     /**
+      * creates different employees from lists
+      * @return a new employee with distinct attributes
+      */
     public static Employee createEmployee(){
-         String[] lastnameList = {"Abbott", "Rivermore", "Longbottom", "Pline", "Fitz", "Abbott", "Rivermore", "Longbottom", "Pline", "Fitz"};
-         String[] firstnameList = {"Adrian", "Marc", "Jean", "Albus", "Peter", "Adrian", "Marc", "Jean", "Albus", "Peter"};
+         // lists to pick from
+         String[] lastnameList = {"Abbott", "Rivermore", "Longbottom", "Pline", "Fitz", "Lumn", "Postia", "Rossi", "Marnor", "Plum"};
+         String[] firstnameList = {"Adrian", "Marc", "Jean", "Albus", "Peter", "Jack", "Percy", "Rose", "Emily", "Georgia"};
          String[] emailList = {"blah", "blah blah", "blah blah blah", "blah blah blah blah", "blah blah blah blah blah",
-                 "blah", "blah blah", "blah blah blah", "blah blah blah blah", "blah blah blah blah blah"};
+                 "email", "email email", "email email email", "email email email email", "email email email email email"};
          int[] ageList = {12, 44, 56, 32, 29, 12, 44, 56, 32, 29};
-         String[] jobTitleList = {"CFO", "CEO", "Dev", "Cook", "Gardener", "CFO", "CEO", "Dev", "Cook", "Gardener"};
+         String[] jobTitleList = {"CFO", "CEO", "Dev", "Cook", "Gardener", "Teacher", "Plumber", "Real estate agent", "Spy", "Singer"};
          String[] addressList = {"blah", "blah blah", "blah blah blah", "blah blah blah blah", "blah blah blah blah blah",
-                 "blah", "blah blah", "blah blah blah", "blah blah blah blah", "blah blah blah blah blah"};;
-         String[] phoneNumberList = {"00000000000","654846515","654351358438","35184834351","687689999",
-                 "00000000000","654846515","654351358438","35184834351","687689999"};
+                 "address", "address blah", "address blah blah", "address blah blah blah", "address blah blah blah blah"};;
+         String[] phoneNumberList = {"000000000","654846515","6543518438","351844351","6879999",
+                 "000111100","77746515","6888888358","3555534351","68333339"};
 
-         int pick = (int) (Math.random() * 10 - 1);
-
+         // create a new empty employee object
          Employee employee = new Employee();
-
-         employee.setJobTitle(jobTitleList[pick]);
-         employee.setPhoneNumber((phoneNumberList[pick]));
-         employee.setLastname(lastnameList[pick]);
-         employee.setFirstname(firstnameList[pick]);
-         employee.setEmail(emailList[pick]);
-         employee.setAge(ageList[pick]);
-         employee.setAddress(addressList[pick]);
+         // adds values to the employee's attributes
+         employee.setJobTitle(jobTitleList[randomInt()]);
+         employee.setPhoneNumber((phoneNumberList[randomInt()]));
+         employee.setLastname(lastnameList[randomInt()]);
+         employee.setFirstname(firstnameList[randomInt()]);
+         employee.setEmail(emailList[randomInt()]);
+         employee.setAge(ageList[randomInt()]);
+         employee.setAddress(addressList[randomInt()]);
 
          return employee;
+    }
+
+     /**
+      * get a random int between 0 & 9
+      * @return a number
+      */
+    public static int randomInt(){
+         return (int) (Math.random() * 10 - 1);
     }
 }
